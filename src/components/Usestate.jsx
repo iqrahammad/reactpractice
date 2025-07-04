3c3c766e2bbf2f285c9cd1487539202339a1cb47
iqrahammad <iqra.aptech001@gmail.com>
2025-07-04 23:52:55 +0500

    second commit

--- src/components/Usestate.jsx
+++ src/components/Usestate.jsx
@@ -14,8 +14,8 @@ const Usestate = () => {
     <div className='Centre  text-center'>
         <h1>Press The Button Below {count}</h1>
        <div className='flex justify-center'>
-        <button onClick={addCount} className='bg-black text-amber-400 font-bold py-3 rounded hover:bg-amber-700 w-20 m-2 p-2'>Add</button>
-        <button onClick={subCount} className='bg-black text-amber-400 font-bold py-3 rounded hover:bg-amber-700 w-20 m-2 p-2'>subtract</button>
+        <button onClick={addCount} className='bg-amber-200 text-amber-400 font-bold py-3 rounded hover:bg-amber-700 w-20 m-2 p-2'>Add</button>
+        <button onClick={subCount} className='bg-amber-200 text-amber-400 font-bold py-3 rounded hover:bg-amber-700 w-20 m-2 p-2'>subtract</button>
         </div> 
     </div>
     </>
